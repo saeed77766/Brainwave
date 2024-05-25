fefce74632b5b580f8a72fe905062509f018ef41
saeed <saeed.babbar1861@gmail.com>
2024-05-25 20:12:17 +0500

    completed notification and working with hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Button from "./Components/Button";
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Header from "./Components/Header";
+import Hero from "./Components/Hero";
 
 const App = () => {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
+        <Hero/>
       </div>
       <ButtonGradient />
     </>
