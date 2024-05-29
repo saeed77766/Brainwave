c2dce3c23666075d6449c216a7357551f3f001e0
saeed <saeed.babbar1861@gmail.com>
2024-05-29 15:07:16 +0500

    Benefits section done

--- src/Components/Benefits.jsx
+++ src/Components/Benefits.jsx
@@ -19,7 +19,7 @@ const Benefits = () => {
             <div
               className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
               style={{
-                backgroundImage: `url(${item.backgroundUrl})`,
+                backgroundImage: `url(/src/${item.backgroundUrl})`,
               }}
               key={item.id}
             >
